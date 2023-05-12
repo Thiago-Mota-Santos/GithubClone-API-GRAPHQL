e1557a6734aa558c1ad8b802e77135da9cfdf944
Thiago Mota Santos <lucimara1422@gmail.com>
2023-05-12 16:11:19 -0300

    chore(jest) rename variable

--- jest.config.ts
+++ jest.config.ts
@@ -15,7 +15,7 @@ export default {
   // cacheDirectory: "C:\\Users\\Th\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls, instances, contexts and results before every test
-  // clearMocks: true,
+  clearMocks: true,
 
 
   // Indicates whether the coverage information should be collected while executing the test
